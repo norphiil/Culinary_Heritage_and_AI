5ce5663c2c26e738bccc9106f461ea1a2c00cdd9
Florent CADET <norphiil@hotmail.fr>
2024-04-11 11:05:54 +0200

    Add the Similar and Disimilar recipes

--- core/views.py
+++ core/views.py
@@ -1,16 +1,12 @@
-import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.cluster import AgglomerativeClustering
-from sklearn.neighbors import NearestNeighbors
 from sklearn.decomposition import PCA
 from django.shortcuts import render
-from sklearn.cluster import KMeans
-from sklearn.manifold import TSNE
 from collections import Counter
 from .models import Recipe
+import numpy as np
 import csv
-
 
 COLOR = {
     0: {
@@ -137,21 +133,6 @@ def clean_all_recipes():
     Recipe.objects.all().delete()
 
 
-def optimal_k(data, max_k=10):
-    if len(data) == 1:
-        return 1
-    distortions = []
-    for k in range(2, max_k+1):
-        kmeans = KMeans(n_clusters=k)
-        kmeans.fit(data)
-        distortions.append(kmeans.inertia_)
-
-    # Find the optimal k
-    delta_distortions = [distortions[i] - distortions[i+1] for i in range(len(distortions)-1)]
-    k_optimal = delta_distortions.index(max(delta_distortions)) + 2  # Add 2 because we start from k=1
-    return k_optimal
-
-
 def tokenize_and_cluster(recipes, selected_cluster: int = None):
     # Tokenize ingredients into binary vectors
     ingredients_list = [recipe.ingredients.split(',') for recipe in recipes]
@@ -160,28 +141,20 @@ def tokenize_and_cluster(recipes, selected_cluster: int = None):
         binary_vector = {ingredient: 1 for ingredient in ingredients}
         ingredients_binary.append(binary_vector)
 
-    # Use DictVectorizer to convert binary vectors into a matrix
     vectorizer: DictVectorizer = DictVectorizer(sparse=False)
     X: np.ndarray = vectorizer.fit_transform(ingredients_binary)
 
-    # Cluster similar dishes
-    # opt_k = optimal_k(X, max_k=5)
-    # kmeans = KMeans(n_clusters=opt_k)  # You can adjust the number of clusters as needed
-    # clusters = kmeans.fit_predict(X)
     hca = AgglomerativeClustering(n_clusters=None, distance_threshold=14)
     clusters = hca.fit_predict(X)
-    # Apply dimensionality reduction using PCA for visualization
+
     pca: PCA = PCA(n_components=2)
     X_reduced: np.ndarray = pca.fit_transform(X)
 
-    # Extract coordinates for plotting
     x_coords = X_reduced[:, 0]
     y_coords = X_reduced[:, 1]
 
-    # Prepare data for plotting
     data: list = []
     new_recipes: list = []
-    print("Selected Cluster: ", selected_cluster is None)
     for i in range(len(recipes)):
         if selected_cluster is None or clusters[i] == selected_cluster:
             data.append({
@@ -197,80 +170,48 @@ def tokenize_and_cluster(recipes, selected_cluster: int = None):
 
 
 def get_similarity_recipes(recipes):
-
-    # Tokenize ingredients into binary vectors
+    # Tokenize ingredients
     ingredients_list = [recipe.ingredients.split(',') for recipe in recipes]
     ingredients_binary = []
     for ingredients in ingredients_list:
         binary_vector = {ingredient: 1 for ingredient in ingredients}
         ingredients_binary.append(binary_vector)
 
-    # Use DictVectorizer to convert binary vectors into a matrix
-    vectorizer = DictVectorizer(sparse=False)
-    X = vectorizer.fit_transform(ingredients_binary)
+    vectorizer: DictVectorizer = DictVectorizer(sparse=False)
+    X: np.ndarray = vectorizer.fit_transform(ingredients_binary)
+    # Use TF-IDF to vectorize ingredients
 
-    # Compute pairwise cosine similarity
+    # Calculate cosine similarity matrix
     similarity_matrix = cosine_similarity(X)
 
-    # Apply dimensionality reduction using PCA for visualization
-    pca = PCA(n_components=2)
-    X_reduced = pca.fit_transform(X)
-
-    # Get indices of recipes sorted by similarity (excluding self-similarity)
-    sorted_indices = similarity_matrix.argsort(axis=1)[:, ::-1][:, 1:]
-    return sorted_indices, similarity_matrix, X_reduced
+    return similarity_matrix
 
 
-def find_similar_recipes(recipes, sorted_indices, similarity_matrix, X_reduced):
-    similarity_scores = similarity_matrix.max(axis=1)
-
-    x_coords = X_reduced[:, 0]
-    y_coords = X_reduced[:, 1]
-    scaled_x_coords = x_coords * similarity_scores
-    scaled_y_coords = y_coords * similarity_scores
-    # Extract most similar recipes
+def find_similar_recipes(recipes, similarity_matrix):
     similar_recipes = []
-    for i, recipe_index in enumerate(sorted_indices):
-        most_similar_index = recipe_index[0]
-        # similar_recipes.append({
-        #     "recipe_name": recipes[i].recipe_name,
-        #     "similar_recipe_name": recipes[int(most_similar_index)].recipe_name,
-        #     "similarity_score": similarity_matrix[i, most_similar_index]
-        # })
-        similar_recipes.append({
-            "x": scaled_x_coords[i],
-            "y": scaled_y_coords[i],
-            "similarity_score": similarity_matrix[i, most_similar_index],
-            "recipe_name": recipes[i].recipe_name
-        })
+    recipes_list = list(recipes)
+    nb = 5
+    for i, recipe in enumerate(recipes_list):
+        similar_indices: int = np.argsort(similarity_matrix[i])[::-1][1:nb+1]
+        similarities = [{"recipe": recipes_list[index], "similarity": similarity_matrix[i][index]} for index in similar_indices if recipes_list[index].recipe_name != recipe.recipe_name]
+        avg_similarity = sum(similarity["similarity"] for similarity in similarities) / nb
+        similar_recipes.append({"recipe": recipe, "avg_similarity": avg_similarity, "data": similarities})
 
+    similar_recipes.sort(key=lambda x: x['avg_similarity'], reverse=True)
     return similar_recipes
 
 
-def find_dissimilar_recipes(recipes, sorted_indices, similarity_matrix, X_reduced):
-    similarity_scores = 1 - similarity_matrix.max(axis=1)
-
-    x_coords = X_reduced[:, 0]
-    y_coords = X_reduced[:, 1]
-    scaled_x_coords = x_coords * similarity_scores
-    scaled_y_coords = y_coords * similarity_scores
-    # Extract most dissimilar recipes
+def find_dissimilar_recipes(recipes, similarity_matrix):
     dissimilar_recipes = []
-    for i, recipe_index in enumerate(sorted_indices):
-        most_dissimilar_index = recipe_index[-1]
-        # dissimilar_recipes.append({
-        #     "recipe_name": recipes[i].recipe_name,
-        #     "dissimilar_recipe_name": recipes[int(most_dissimilar_index)].recipe_name,
-        #     "dissimilarity_score": similarity_matrix[i, most_dissimilar_index]
-        # })
+    recipes_list = list(recipes)
+    nb = 5
+    for i, recipe in enumerate(recipes):
+        dissimilar_indices: int = np.argsort(similarity_matrix[i])[::-1][-nb:]
+        dissimilarities = [{"recipe": recipes_list[index], "similarity": similarity_matrix[i][index]} for index in dissimilar_indices if recipes_list[index].recipe_name != recipe.recipe_name]
+        avg_dissimilarity = sum(dissimilarity["similarity"] for dissimilarity in dissimilarities) / nb
+        dissimilar_recipes.append({"recipe": recipe, "avg_dissimilarity": avg_dissimilarity, "data": dissimilarities})
 
-        dissimilar_recipes.append({
-            "x": scaled_x_coords[i],
-            "y": scaled_y_coords[i],
-            "similarity_score": similarity_matrix[i, most_dissimilar_index],
-            "recipe_name": recipes[i].recipe_name
-        })
-
+    dissimilar_recipes.sort(key=lambda x: x['avg_dissimilarity'], reverse=False)
     return dissimilar_recipes
 
 
@@ -295,7 +236,7 @@ def index(request):
     selected_recipe: str = request.GET.get('recipe', None)
     selected_dish: str = request.GET.get('dish', None)
     selected_cluster: str = request.GET.get('clusters', None)
-    if selected_cluster.isdigit():
+    if selected_cluster and selected_cluster.isdigit():
         selected_cluster = int(selected_cluster)
     else:
         selected_cluster = None
@@ -303,11 +244,13 @@ def index(request):
         recipes = Recipe.objects.filter(dish_name=selected_dish)
         recipes_selector = recipes
 
-        sorted_indices, similarity_matrix, X_reduced = get_similarity_recipes(recipes)
-        similar_recipes = find_similar_recipes(recipes, sorted_indices, similarity_matrix, X_reduced)
-        dissimilar_recipes = find_dissimilar_recipes(recipes, sorted_indices, similarity_matrix, X_reduced)
+        similarity_matrix = get_similarity_recipes(recipes)
+        similar_recipes = find_similar_recipes(recipes, similarity_matrix)
+        dissimilar_recipes = find_dissimilar_recipes(recipes, similarity_matrix)
         if selected_recipe:
             recipes = recipes.filter(recipe_name=selected_recipe)
+            similar_recipes = [recipe for recipe in similar_recipes if recipe['recipe_name'] == selected_recipe]
+            dissimilar_recipes = [recipe for recipe in dissimilar_recipes if recipe['recipe_name'] == selected_recipe]
     else:
         recipes_selector = recipes
         similar_recipes = []
@@ -321,19 +264,10 @@ def index(request):
     scatter_chart_graph_subtitle = "Based on selected filters"
     scatter_chart_x_title = ""
     scatter_chart_y_title = ""
-
-    similarity_bar_chart_graph_title = "Recipe Similarity"
-    similarity_bar_chart_graph_subtitle = "Based on selected filters"
-    similarity_bar_chart_x_title = "Recipe"
-    similarity_bar_chart_y_title = "Similarity Score"
-
-    dissimilarity_bar_chart_graph_title = "Recipe Dissimilarity"
-    dissimilarity_bar_chart_graph_subtitle = "Based on selected filters"
-    dissimilarity_bar_chart_x_title = "Recipe"
-    dissimilarity_bar_chart_y_title = "Dissimilarity Score"
     data, clusters, new_recipes = tokenize_and_cluster(recipes_selector, selected_cluster=selected_cluster)
-
-    print("Selected Cluster: ", selected_cluster)
+    print("Similar Recipes", similar_recipes)
+    print(len(similar_recipes))
+    print(len(recipes))
     bar_data = get_bar_data(new_recipes)
     return render(request, 'index.html', {
         "bar_chart_title": bar_chart_graph_title,
@@ -348,20 +282,11 @@ def index(request):
         "scatter_chart_y_title": scatter_chart_y_title,
         "scatter_chart_data": data,
 
+        "similar_recipes": similar_recipes,
+        "dissimilar_recipes": dissimilar_recipes,
+
         "clusters": [{"cluster": int(cluster), "color": COLOR[int(cluster)]["name"]} for cluster in range(max(clusters)+1)],
         "selected_cluster": selected_cluster,
-
-        "similarity_bar_chart_title": similarity_bar_chart_graph_title,
-        "similarity_bar_chart_subtitle": similarity_bar_chart_graph_subtitle,
-        "similarity_bar_chart_x_title": similarity_bar_chart_x_title,
-        "similarity_bar_chart_y_title": similarity_bar_chart_y_title,
-        "similarity_bar_chart_data": similar_recipes,
-
-        "dissimilarity_bar_chart_title": dissimilarity_bar_chart_graph_title,
-        "dissimilarity_bar_chart_subtitle": dissimilarity_bar_chart_graph_subtitle,
-        "dissimilarity_bar_chart_x_title": dissimilarity_bar_chart_x_title,
-        "dissimilarity_bar_chart_y_title": dissimilarity_bar_chart_y_title,
-        "dissimilarity_bar_chart_data": dissimilar_recipes,
 
         "dishes": Recipe.objects.values_list('dish_name', flat=True).distinct(),
         "recipes": recipes_selector,
